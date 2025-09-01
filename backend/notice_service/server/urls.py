65b893d82bbe73bd2c21b770d866bb44c43797c7
zdaf <zdaf_lovestudy@163.com>
2025-09-01 11:57:08 +0800

    删微服务

--- backend/notice_service/server/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path, include
-from notice_app import urls as notice_urls
-
-urlpatterns = [
-    path('api/', include(notice_urls)),
-    path('health/', lambda r: HttpResponse(status=200)),  # 健康检查
-]
