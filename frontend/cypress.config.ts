ccc41ef2a1a113f1961a0a2c7f5d0d5ec06c1df0
Moxing04 <145923665+Moxing04@users.noreply.github.com>
2025-09-01 15:46:13 +0800

    GUIdebug

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-  baseUrl: 'http://localhost:5173',
+  baseUrl: 'http://127.0.0.1:8080',
     setupNodeEvents(on, config) {
     },
     specPattern: "cypress/e2e/**/*.cy.{js,ts}", // 可根据你的项目调整
