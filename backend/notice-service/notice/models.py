164f3c176fff7087bd9f38bf3be04eb91f03bf14
luhan <w13161412991@163.com>
2025-09-02 11:00:23 +0800

    公告微服务

--- backend/notice-service/notice/models.py
+++ backend/notice-service/notice/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 class Notice(models.Model):
     NOTICE_TYPES = (
@@ -13,19 +14,18 @@ class Notice(models.Model):
     type = models.SmallIntegerField('类型', choices=NOTICE_TYPES, default=2)
     date = models.DateTimeField('发布时间', auto_now_add=True)
     is_top = models.BooleanField('是否置顶', default=False)
-    
-    # 使用ID替代外键
-    author_id = models.IntegerField('作者ID')
-    experiment_id = models.IntegerField('关联实验ID', null=True, blank=True)
-    
-    # 使用JSONField存储已读用户ID
-    reader_ids = models.JSONField('已读用户ID列表', default=list)
+    author_id = models.IntegerField('作者ID')  # 改为普通字段存储
+    experiment_id = models.IntegerField('关联实验ID', null=True, blank=True)  # 改为普通字段存储
+    readers = models.JSONField('已读用户ID列表', default=list)  # 存储用户ID列表
 
     class Meta:
+        verbose_name = '公告'
+        verbose_name_plural = '公告管理'
         ordering = ['-is_top', '-date']
 
     def __str__(self):
         return self.title
 
+    # 检查用户是否已读此公告
     def is_read_by(self, user_id):
-        return user_id in self.reader_ids
+        return user_id in self.readers
