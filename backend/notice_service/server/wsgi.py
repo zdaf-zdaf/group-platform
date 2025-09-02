9e300248d3554ba0c492da7eef9f4ba1595bd47e
zdaf <zdaf_lovestudy@163.com>
2025-09-03 00:39:55 +0800

    notice

--- backend/notice_service/server/wsgi.py
+++ backend/notice_service/server/wsgi.py
@@ -1,5 +1,5 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'notice_service.settings')
 application = get_wsgi_application()  
